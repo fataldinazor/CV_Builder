65796809dd808d9c0d87f8396c521076fdb99620
FIXED-TERM Joshi Pushpesh (MS/ECJ2-PS) <huo3kor@bosch.com>
2024-06-26 10:33:53 +0530

    added css files

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import GeneralInformation from "./GeneralInformation"
 import Education from "./Education"
 import Experience from "./Experience"
+import "../styles/Information.css"
+
 
 export default function Information(props){
 
     return(
         
-        <div className="informationForm">
+        <div className="information">
             <GeneralInformation setGeneralInfo={props.setGeneralInfo}/>
             <Education setEducationInfo={props.setEducationInfo} />
             <Experience setExperienceInfo={props.setExperienceInfo}/>
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,8 +1,11 @@
+import "../styles/Preview.css"
 export default function Preview(){
 
     return(
         <div className="previewDiv">
-            
+            <div className="previewPage">
+
+            </div>
         </div>
     )
 }
